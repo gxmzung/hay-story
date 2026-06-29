e6343b98640f08733f3ac8c959ac29dfcc079797
lee youngjun <leeyj4748@naver.com>
2026-06-29 17:40:00 +0900

    style: simplify home into commerce landing

--- app/page.tsx
+++ app/page.tsx
@@ -3,28 +3,18 @@ import CategoryTabs from "../src/components/CategoryTabs";
 import MainBanner from "../src/components/MainBanner";
 import QuickMenu from "../src/components/QuickMenu";
 import BrandCircleList from "../src/components/BrandCircleList";
-import MomentSelector from "../src/components/MomentSelector";
-import AiStyleFinder from "../src/components/AiStyleFinder";
-import CustomOrder from "../src/components/CustomOrder";
 import ProductGrid from "../src/components/ProductGrid";
-import Footer from "../src/components/Footer";
 import DesignerTalk from "../src/components/DesignerTalk";
+import Footer from "../src/components/Footer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#f7f3ee] text-[#1f1f1f]">
+    <main className="min-h-screen bg-white text-[#1f1f1f]">
       <Header />
       <CategoryTabs />
       <MainBanner />
       <QuickMenu />
       <BrandCircleList />
-
-      <section className="flex flex-col items-center bg-[#f7f3ee] px-6 py-10 text-center">
-        <MomentSelector />
-        <AiStyleFinder />
-        <CustomOrder />
-      </section>
-
       <ProductGrid />
       <DesignerTalk />
       <Footer />
